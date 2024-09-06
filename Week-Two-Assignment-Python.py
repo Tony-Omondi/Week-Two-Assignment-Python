732a50f7c25e632c18c7675f8d841bc94687576e
Tony-omondi <nitonito598@gmail.com>
2024-09-07 00:21:20 +0300

    first commit

---
+++ Week-Two-Assignment-Python.py
@@ -0,0 +1,12 @@
+
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+my_list.insert(1, 15)
+
+my_list += [50, 60, 70]
+print("Index of 30:", my_list.index(30))
